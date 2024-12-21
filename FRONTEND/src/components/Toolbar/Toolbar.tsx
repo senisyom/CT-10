6177b9d595b586efadde78ae9ca1da3bb0eb3104
Begmatov Almaz <lolkek13@inbox.ru>
2024-12-21 12:08:36 +0600

    [FRONTEND] created toolbar and layot

---
+++ FRONTEND/src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+const Toolbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <NavLink to="/" className="navbar-brand">
+          News
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Toolbar;
---
+++ FRONTEND/src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Toolbar from "../Toolbar/Toolbar";
+
+const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <header>
+        <Toolbar />
+      </header>
+      <main className="container-fluid">{children}</main>
+    </>
+  );
+};
+
+export default Layout;
